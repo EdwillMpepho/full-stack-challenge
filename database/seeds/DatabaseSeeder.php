df4c306a1b5e5e2d89f8f75735fd147c70286510
edwill-themba <mpephothemba@yahoo.com>
2023-09-18 18:29:03 -0400

    finish adding testing data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use App\User;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserSeeder::class);
+      //$this->call(UserSeeder::class);
+      $users = [
+        [
+            'name' => 'Branko Ilic',
+            'email' => 'branckoilic@gmail.com',
+            'password' =>  bcrypt('secret'),
+            'role' => 0,
+            'banned' =>false
+        ],
+        [
+            'name' => 'Mpepho Themba',
+            'email' => 'mpephothemba@gmail.com',
+            'password' =>  bcrypt('secret'),
+            'role' => 1,
+            'banned' =>false
+        ],
+        [
+            'name' => 'Noluthando Zulu',
+            'email' => 'noluthandozulu@gmail.com',
+            'password' =>  bcrypt('secret'),
+            'role' => 2,
+            'banned' =>false
+        ],
+        [
+            'name' => 'Jacob Harison',
+            'email' => 'jacobharrison@yahoo.com',
+            'password' =>  bcrypt('secret'),
+            'role' => 2,
+            'banned' =>false
+        ],
+        [
+            'name' => 'Brandy Thompson',
+            'email' => 'bradythompson@gmail.com',
+            'password' =>  bcrypt('secret'),
+            'role' => 2,
+            'banned' =>false
+        ],
+    ];
+    foreach($users as $user)
+    {
+        User::create($user);
     }
+  }
 }
